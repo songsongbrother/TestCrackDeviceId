0c45a3e99547cd45bf7a9494d5311fdd87905838
chensongsong <chensongsong@geetest.com>
2019-09-10 14:02:56 +0800

    [feature/0.1.0][update][ 添加 xhook 模块 ]

--- app/src/main/java/com/song/testcrackdeviceid/MainActivity.java
+++ app/src/main/java/com/song/testcrackdeviceid/MainActivity.java
@@ -29,12 +29,58 @@ public class MainActivity extends AppCompatActivity {
                 option.setOrganization(DEBUG_ORG);
                 option.setChannel("test");
                 SmAntiFraud.create(getApplicationContext(), option);
+                com.qiyi.xhook.XHook.getInstance().refresh(false);
 
-                String deviceId = SmAntiFraud.getDeviceId();
+//                String deviceId = SmAntiFraud.getDeviceId();
 //
 //                Toast.makeText(getApplicationContext(), "smid: " + deviceId, Toast.LENGTH_SHORT).show();
 //                Log.e(TAG, "smid: " + deviceId);
             }
         });
+
+        //load xhook
+        com.qiyi.xhook.XHook.getInstance().init(this.getApplicationContext());
+        if (!com.qiyi.xhook.XHook.getInstance().isInited()) {
+            return;
+        }
+        //com.qiyi.xhook.XHook.getInstance().enableDebug(true); //default is false
+        //com.qiyi.xhook.XHook.getInstance().enableSigSegvProtection(false); //default is true
+
+        //load and run your biz lib (for register hook points)
+        com.qiyi.biz.Biz.getInstance().init();
+        com.qiyi.biz.Biz.getInstance().start();
+
+        //xhook do refresh
+        com.qiyi.xhook.XHook.getInstance().refresh(false);
+
+        //load and run the target lib
+//        com.qiyi.test.Test.getInstance().init();
+//        com.qiyi.test.Test.getInstance().start();
+//        try {
+//            Thread.sleep(200);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        //xhook do refresh again
+//        com.qiyi.xhook.XHook.getInstance().refresh(false);
+
+        //xhook do refresh again for some reason,
+        //maybe called after some System.loadLibrary() and System.load()
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    com.qiyi.xhook.XHook.getInstance().refresh(true);
+
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
